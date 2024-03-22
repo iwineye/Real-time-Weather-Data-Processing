1c114773a48515500c1b25c8167965815ce12cfa
VIN <96835772+iwineye@users.noreply.github.com>
2024-03-22 21:55:53 +0530

    Create snowflake_connector.py

---
+++ kafka-producer/snowflake_connector.py
@@ -0,0 +1,17 @@
+# Weather API configuration
+RAPID_API_KEY = '<RAPID_API_KEY>'
+API_URL = "https://open-weather13.p.rapidapi.com/city/newdelhi".format(RAPID_API_KEY)
+
+# Kafka configuration
+KAFKA_SERVER = 'localhost:9092'
+KAFKA_TOPIC = 'weather_topic'
+
+# Snowflake configuration
+SNOWFLAKE_CONFIG = {
+    'user': 'USERNAME',
+    'password': 'PASSWORD',
+    'account': 'ACCOUNT',
+    'warehouse': 'WAREHOUSE',
+    'database': 'DATABASE',
+    'schema': 'SCHEMA'
+}
