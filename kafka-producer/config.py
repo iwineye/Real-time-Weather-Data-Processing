ef4160af6b305dbf65cb4edbe1040f89a8843059
VIN <96835772+iwineye@users.noreply.github.com>
2024-03-22 21:24:10 +0530

    Create config.py

---
+++ kafka-producer/config.py
@@ -0,0 +1,16 @@
+# Weather API configuration
+RAPID_API_KEY = '<RAPID_API_KEY>'
+
+# Kafka configuration
+KAFKA_SERVER = 'localhost:9092'
+KAFKA_TOPIC = 'weather_topic'
+
+# Snowflake configuration
+SNOWFLAKE_CONFIG = {
+    'user': '<USERNAME>',
+    'password': '<PASSWORD>',
+    'account': '<ACCOUNT>',
+    'warehouse': '<WAREHOUSE>',
+    'database': '<DATABASE>',
+    'schema': '<SCHEMA>'
+}
